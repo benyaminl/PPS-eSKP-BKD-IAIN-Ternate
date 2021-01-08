bd830f0c2c59d3ee7656d673cabfd01f39b7f300
Nabillah Annisa <bellasnabillah@gmail.com>
2021-01-08 19:57:12 +0700

    Model ne kancrit

---
+++ app/Models/BKDPendidikan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BKDPendidikan extends Model
+{
+	protected $table = 'rbkd';
+	//protected $primaryKey = 'No';
+	protected $fillable = ['Bidang','Jenis_Kegiatan','Bukti_Penugasan','SKS_RBKD'];
+    //use HasFactory;
+}
---
+++ app/Models/IdentitasBKD.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IdentitasBKD extends Model
+{
+	protected $table = 'identitas_dosen';
+    //use HasFactory;
+}
